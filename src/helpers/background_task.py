880a0ceadea681cb216e8f875476c489cfd33bcf
Giorgos Gagrinas <gagrinas.g@gmail.com>
2023-12-16 20:21:43 +0200

    Added emojis to messages

--- src/helpers/background_task.py
+++ src/helpers/background_task.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import emoji
 
 from src.football_sdk.screenshot import screenshot_fixture
 
@@ -10,10 +11,16 @@ class BackgroundRunner:
     async def send_message(self,seconds,football_client,telegram_client):
         while True:
             prediction , fixture_id = await football_client.get_live_prediction_for_ongoing_match()
+            # prediction = 'test'
+            # fixture_id = 1037532
             # Found fixture to post
+            emojis_before = emoji.emojize('🔥🔥 Live Bet\n💯')
+            emojis_after = emoji.emojize('\n ✅✅✅✅✅✅')
+
+            caption = ''.join([emojis_before,prediction,emojis_after])
             if fixture_id:
                 screenshot_path = await screenshot_fixture(fixture_id)
-                await telegram_client.send_message_with_photo(prediction,screenshot_path)
+                await telegram_client.send_message_with_photo(caption,screenshot_path)
                 # await telegram_client.send_message(prediction)
 
                 # Delete the screenshot
--- src/redis_client/redis_connector.py
+++ src/redis_client/redis_connector.py
@@ -6,7 +6,7 @@ from redis import Redis
 class RedisConnector:
     def __init__(self, host=None, port=None, db=None):
         # host = "redis"  # Use the service name from Docker Compose
-        host = host or "127.0.0.1"
+        host =  "127.0.0.1"
         port = 6379
         db = 0
 
